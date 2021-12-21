ec3c2eb068bc84f87c51abbfd2a394ef55f9d8e8
Brian D. Baker <brian.devs.baker@gmail.com>
2021-12-20 20:29:01 -0500

    Fixed bug with image links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Navigation from './Navigation'
 function Header({currentPage, setCurrentPage}) {
 
   return (
-    <header style={{backgroundImage: "url('/img/pxfuel.com.jpg')" }} className="min-h-fit h-40 bg-yellow-400 mb-6">
+    <header style={{backgroundImage: "url('img/pxfuel.com.jpg')" }} className="min-h-fit h-40 bg-yellow-400 mb-6">
       <div className='container flex flex-col justify-center items-start lg:flex-row lg:items-center lg:justify-between h-full px-4'>
         <h1 className='text-5xl md:text-6xl text-black font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-yellow-900 to-stone-900 lg:h-16'>Brian D. Baker</h1>
         <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage}/>
